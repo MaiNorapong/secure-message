af82d411d8315fa70aa7a8482fcbdfa108b0260b
MaiNorapong <54196368+MaiNorapong@users.noreply.github.com>
2021-02-18 11:58:03 +0700

    Add OAuth using GitHub
    - Remove username input for posting message

--- src/main/java/ku/message/controller/MessageController.java
+++ src/main/java/ku/message/controller/MessageController.java
@@ -2,6 +2,8 @@ package ku.message.controller;
 
 import ku.message.model.Message;
 import ku.message.repository.MessageRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,7 +30,8 @@ public class MessageController {
     }
 
     @PostMapping("/message")
-    public String postMessage(@ModelAttribute Message message, Model model) {
+    public String postMessage(@ModelAttribute Message message, Model model, @AuthenticationPrincipal OAuth2User user) {
+        message.setUser(user.getAttribute("name"));
         repository.save(message);
         model.addAttribute("message", repository.findAll());
         return "redirect:message";
